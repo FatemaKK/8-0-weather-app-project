a8c47a49df61b6f44b112a0b76a6a57fc4e432c8
Fatema Kabir <fatema.kabir@pursuit.org>
2021-10-11 19:30:26 -0400

    sidebar-list

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
   
     const location = event.target.location.value;
-    fetch(`https://wttr.in/~${location}?format=j1`)
+    fetch(`https://wttr.in/${location}?format=j1`)
     .then((response) => response.json())
     .then((weather) => {
-        // console.log(weather);
+        if(location.length === 0){
+            throw new Error;
+        }
   
-        let displayArea = document.querySelector(".display");
+        let displayArea = document.querySelector(".print");
         let area = weather.nearest_area[0].areaName[0].value;
         let region = weather.nearest_area[0].region[0].value;
         let country = weather.nearest_area[0].country[0].value;
@@ -28,9 +30,9 @@ document.querySelector("form").addEventListener("submit", (event) => {
             
         today.innerHTML = 
         `<h3>Today</h3>
-        <p><strong>Average Temp:</strong> ${averageTemp1}</p>
-        <p><strong>Min Temp:</strong> ${minTemp1}</p>
-        <p><strong>Max Temp:</strong> ${maxTemp1}</p>`;    
+        <p><strong>Average Temp:</strong> ${averageTemp1}°F</p>
+        <p><strong>Min Temp:</strong> ${minTemp1}°F</p>
+        <p><strong>Max Temp:</strong> ${maxTemp1}°F</p>`;    
 
         let tomorrow = document.querySelector(".tomorrow");
         let averageTemp2 = weather.weather[1].avgtempF;
@@ -39,9 +41,9 @@ document.querySelector("form").addEventListener("submit", (event) => {
         
         tomorrow.innerHTML = 
         `<h3>Tomorrow</h3>
-        <p><strong>Average Temp:</strong> ${averageTemp2}</p>
-        <p><strong>Min Temp:</strong> ${minTemp2}</p>
-        <p><strong>Max Temp:</strong> ${maxTemp2}</p>`; 
+        <p><strong>Average Temp:</strong> ${averageTemp2}°F</p>
+        <p><strong>Min Temp:</strong> ${minTemp2}°F</p>
+        <p><strong>Max Temp:</strong> ${maxTemp2}°F</p>`; 
 
         let after = document.querySelector(".after");
         let averageTemp3 = weather.weather[2].avgtempF;
@@ -50,10 +52,22 @@ document.querySelector("form").addEventListener("submit", (event) => {
     
         after.innerHTML = 
         `<h3>Day After Tomorrow</h3>
-        <p><strong>Average Temp:</strong> ${averageTemp3}</p>
-        <p><strong>Min Temp:</strong> ${minTemp3}</p>
-        <p><strong>Max Temp:</strong> ${maxTemp3}</p>`; 
+        <p><strong>Average Temp:</strong> ${averageTemp3}°F</p>
+        <p><strong>Min Temp:</strong> ${minTemp3}°F</p>
+        <p><strong>Max Temp:</strong> ${maxTemp3}°F</p>`; 
+        
+        let printHistory = document.querySelector(".history ul");
+        let historyList = document.querySelector("li");
+        console.log(historyList);
+        if (historyList.textContent === "No previous searches."){
+            printHistory.innerHTML = `<li><a href="#" value="${location}" name="test">${location}</a> - ${currentTemp}°F</li>`;
+        } else {
+            printHistory.innerHTML += `<li><a href=#" value="${location}" name="test">${location}</a> - ${currentTemp}°F</li>`;
+        }
     })
     .catch(console.log);  
     event.target.reset(); 
 });
+
+
+
